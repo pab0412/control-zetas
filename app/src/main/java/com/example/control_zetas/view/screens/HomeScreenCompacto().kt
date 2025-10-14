d4211a0b0b833f9c5d5b042df0c56b9b72a249e0
pab0412 <pabl.sepulvedaa@duocuc.cl>
2025-10-14 12:55:50 -0300

    Arreglado 2

---
+++ app/src/main/java/com/example/control_zetas/view/screens/HomeScreenCompacto().kt
@@ -0,0 +1,53 @@
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.control_zetas.R
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreenCompacto(){
+    Scaffold(
+        topBar = {
+            TopAppBar(title = { Text("Mi App Kotlin") })
+        }
+    ) { innerPadding ->
+        Column (
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(20.dp)
+        ){
+            Text(text = "Bienvenido")
+            Button(onClick = {}) {
+                Text("Presioname")
+            }
+
+            Image(
+                painter = painterResource(id = R.drawable.logo),
+                contentDescription = "Logo",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(150.dp),
+                contentScale = ContentScale.Fit
+            )
+
+
+        }
+    }
+}
